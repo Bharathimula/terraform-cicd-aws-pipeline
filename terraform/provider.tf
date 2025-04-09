b5db7aca2c5e13ebc276a348b3a27bae417303dc
Bharathimula <bharathim273@gmail.com>
2025-04-09 16:37:47 +0530

    Updated provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "my_instance" {
-  ami           = "ami-0abcdef1234567890"
+  ami           = "ami-0967e5535761d839e"
   instance_type = var.instance_type
   key_name      = var.key_name
 }
